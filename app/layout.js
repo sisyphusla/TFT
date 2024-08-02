c312f933495e564336740ff436e800c73950a855
Trent <trent@kooco.com.tw>
2024-08-03 00:23:25 +0800

    feat:新增google 分析組件

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import GoogleAnalytics from './GoogleAnalytics';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 const inter = Inter({ subsets: ['latin'] });
 
